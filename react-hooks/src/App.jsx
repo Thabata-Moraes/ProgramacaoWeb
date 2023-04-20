10df52bc2b464564f2df5f28bfc1f6cc2bfb72ff
Thabata Moraes <90910437+Thabata-Moraes@users.noreply.github.com>
2023-04-20 12:24:27 +0000

    aula 20/04

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Exercicio01 from './exercicios/01'
+import Exercicio02 from './exercicios/02'
+import Home from './pages/Home'
+
 
 function App() {
 
@@ -8,7 +11,7 @@ function App() {
     <div className="App">
       <h1>Exercícios de React Hooks</h1>
       <BrowserRouter>
-      <ul>
+      <ul className='menu'>
         <li>
           <Link to="/01">Exercício 01</Link>
         </li>
@@ -30,8 +33,9 @@ function App() {
       </ul>
       <hr/> 
         <Routes>
+          <Route path='/' element={<Home/>}/>
           <Route path="01" element={<Exercicio01/>}/>
-          <Route path='02'/>
+          <Route path='02'element={<Exercicio02/>}/>
           <Route path='03'/>
           <Route path='04'/>
           <Route path='05'/>
