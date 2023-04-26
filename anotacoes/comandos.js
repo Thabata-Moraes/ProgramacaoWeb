1422bbd4a95b74cff743d267c66de292a8ccc0c1
Thabata Moraes <90910437+Thabata-Moraes@users.noreply.github.com>
2023-04-26 13:59:21 +0000

    aula dia 26/04

--- anotacoes/comandos.js
+++ anotacoes/comandos.js
@@ -7,3 +7,24 @@
 
 
 //Renderiza uma vez só por pág ou por aplicação?
+
+
+/*
+
+    Kent C. Dodds - bacana para estudar, pessoa em que Fausto se baseia. 
+
+    Local storage armazena dados, só é perdido quando limpar o cache do navegador
+    session storage faz essa função enquanto o navegador não é fechado  
+
+    useEffect -> efeito colateral realizado a partir de outra ação
+
+    operador ?? -> se a primeira parte for verdade, se existir, ela será executada, caso não a segunda será
+    window.localStorage.getItem('name') ?? initialName 
+
+
+    SPA -> single page aplication -> recarrega só parte 
+    
+    
+    Estado elevado: deixa no pai os estados para que possa ser compartilhado entre filhos 
+    Estado colocado: quando o compartilhamento de um estado não é necessário, dessa forma ele permanece no mesmo component. "Rebaixa".
+*/
