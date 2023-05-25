788aca8f65843c7bdb536b802f1f5c5d65faaa59
Thabata Moraes <90910437+Thabata-Moraes@users.noreply.github.com>
2023-05-25 12:12:35 +0000

    (25/05) Continuação da listagem de clientes

--- Karangos/src/pages/CustomersList.jsx
+++ Karangos/src/pages/CustomersList.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { Typography } from '@mui/material'
-import Box from '@mui/material/Box';
+import { IconButton, Paper, Typography } from '@mui/material'
 import { DataGrid } from '@mui/x-data-grid';
+import { format } from 'date-fns';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 export default function CustomerList(){
     const [state, setState] = useState({
@@ -32,47 +34,66 @@ export default function CustomerList(){
     }
 
     const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
+        {   field: 'id', headerName: 'ID', width: 90 },
         {
-          field: 'firstName',
-          headerName: 'First name',
-          width: 150,
-          editable: true,
+            field: 'name',
+            headerName: 'Nome',
+            width: 300,
         },
         {
-          field: 'lastName',
-          headerName: 'Last name',
-          width: 150,
-          editable: true,
+            field: 'ident_document',
+            headerName: 'CPF',
+            align: 'center',
+            headerAlign: 'center',
+            width: 150,
         },
         {
-          field: 'age',
-          headerName: 'Age',
-          type: 'number',
-          width: 110,
-          editable: true,
+            field: 'birth_date',
+            headerName: 'Data nasc.',
+            headerAling: 'center',
+            width: 100,
+            valueFormatter: (params) => {
+                if(params.value) return format(new Date(params.value), 'dd/MM/yyyy')
+                else return ''
+            }
         },
         {
-          field: 'fullName',
-          headerName: 'Full name',
-          description: 'This column has a value getter and is not sortable.',
-          sortable: false,
-          width: 160,
-          valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
-      ];
-      
-      const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+            field: 'city',
+            headerName: 'Município/UF',
+            width: 300,
+            // colocando dois campos na mesma célula
+            valueGetter: params => params.row.city + '/' + params.row.uf 
+        }, {
+            field: 'phone',
+            headerName: 'Celular',
+            headerAling: 'center',
+            aling: 'center',
+            width: 150,
+          }, {
+            field: 'email',
+            headerName: 'Email',
+            width: 200,
+          },{
+            field: 'edit',
+            headerName: 'Editar',
+            headerAlign: 'center',
+            align: 'center',
+            width: 90,
+            renderCell: params => 
+            <IconButton aria-label='Editar'>
+                <EditIcon/>
+            </IconButton>
+          },{
+            field: 'delete',
+            headerName: 'Excluir',
+            headerAlign: 'center',
+            align: 'center',
+            width: 90,
+            renderCell: params => 
+            <IconButton aria-label='Deletar'>
+                <DeleteForeverIcon color='error'/>
+            </IconButton>
+          },
       ];
 
     return (
@@ -81,9 +102,9 @@ export default function CustomerList(){
                 Listagem de clientes
             </Typography>
 
-            <Box sx={{ height: 400, width: '100%' }}>
+            <Paper elevation={4} sx={{ height: 400, width: '100%', mt: '50px' }}>
                 <DataGrid
-                    rows={rows}
+                    rows={customers}
                     columns={columns}
                     initialState={{
                     pagination: {
@@ -96,11 +117,7 @@ export default function CustomerList(){
                     checkboxSelection
                     disableRowSelectionOnClick
                 />
-            </Box>
-
-            <div>
-                {JSON.stringify(customers)}
-            </div>
+            </Paper>
         </>
     )
 }
